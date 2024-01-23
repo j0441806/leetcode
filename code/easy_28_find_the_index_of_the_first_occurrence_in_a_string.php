71cb47f1eac29f0f10b1f1713179bec55580f2f9
j0441806 <0441806j@gmail.com>
2024-01-23 20:46:47 +0900

    Add easy_28_find_the_index_of_the_first_occurrence_in_a_string

--- code/easy_28_find_the_index_of_the_first_occurrence_in_a_string.php
+++ code/easy_28_find_the_index_of_the_first_occurrence_in_a_string.php
@@ -14,30 +14,75 @@ class Solution {
      * @return Integer
      */
     function strStr($haystack, $needle) {
-        $matchLoc = 0;
-        $result = -1;
 
-        for($i = 0;$i < strlen($haystack);$i++) {
-            if ($haystack[$i] === $needle[$matchLoc]) {
-                // echo $i, $matchLoc;
-                if ($matchLoc === 0) {
-                    $result = $i;
+        $haystackLen = strlen($haystack);
+        $needleLen = strlen($needle);
+
+        // if ($haystack === $needle) {
+        //     return 0;
+        // }
+
+        for($i = 0;$i < $haystackLen;$i++) {
+            $match = true;
+
+            for ($j = 0; $j < $needleLen; $j++) {
+                // echo $i, $j;
+                if ($haystack[$i + $j] !== $needle[$j]) {
+                    $match = false;
+                    break;
                 }
-                if ($matchLoc === strlen($needle) - 1) {
-                    // echo $result;
-                    return $result;
-                }
-                $matchLoc++;
-                
             }
+
+            if ($match) {
+                return $i;
+            }
+
+
+            // if ($haystack[$i] === $needle[$matchLoc]) {
+            //     // echo $i, $matchLoc;
+            //     $find = true;
+            //     if ($matchLoc === $needleLen - 1) {
+            //         // echo $i, $matchLoc;
+            //         $result = $i - $matchLoc;
+            //         return $result;
+            //     }
+                
+            //     $matchLoc++;
+            // } else {
+            //     if ($find === true) {
+            //         // echo $haystackLen - $i + 1;
+            //         if ($haystackLen - $i + 1 > $needleLen) {
+            //             echo $haystack[$i];
+            //             echo $i;
+            //             $matchLoc = 0;
+            //             $find = false;
+            //         } else {
+            //             return $result;
+            //         }
+            //     }
+            // }
         }
-        return $result;
+        // return $result;
+        return -1;
         
         
     }
 }
 // @lc code=end
-$haystack = "aaa";
-$needle = "aaaa";
+$haystack = "sadbutsad";
+$needle = "sad";
+// $haystack = "leetcode";
+// $needle = "leeto";
+// $haystack = "hello";
+// $needle = "ll";
+// $haystack = "mississippi";
+// $needle = "issip";
 echo (new Solution())->strStr($haystack, $needle);
+
+// 2024/1/23
+// この問題の場合、二重ループを使用することが鍵
+// $needleの途中でマッチしなかったらfalseにして、needleのループを抜ける
+// if ($haystack[$i + $j] !== $needle[$j]) {
+// かえって冗長になる場合、return -1;を記述しないことにこだわらない
+
 
