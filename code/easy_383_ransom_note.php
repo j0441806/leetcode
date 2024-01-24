fbddeea6afafa8ab664c5d2353508884ae518467
j0441806 <0441806j@gmail.com>
2024-01-24 20:05:55 +0900

    Add easy_383_ransom_note

---
+++ code/easy_383_ransom_note.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * @lc app=leetcode id=383 lang=php
+ *
+ * [383] Ransom Note
+ */
+
+// @lc code=start
+class Solution {
+
+    /**
+     * @param String $ransomNote
+     * @param String $magazine
+     * @return Boolean
+     */
+    function canConstruct($ransomNote, $magazine) {
+        $map = [];
+
+        foreach ($ransomNote as $letter) {
+            // if array_key_exists()
+        }
+        
+    }
+}
+// @lc code=end
+
