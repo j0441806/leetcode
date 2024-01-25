db455e40e695d29fb4314d4ba6d2df7345c73172
j0441806 <0441806j@gmail.com>
2024-01-25 16:23:23 +0900

    Modify easy_383_ransom_note

--- code/easy_383_ransom_note.php
+++ code/easy_383_ransom_note.php
@@ -14,13 +14,34 @@ class Solution {
      * @return Boolean
      */
     function canConstruct($ransomNote, $magazine) {
-        $map = [];
-
-        foreach ($ransomNote as $letter) {
-            // if array_key_exists()
+        if (strlen($magazine) < strlen($ransomNote)) {
+            return false;
         }
+        
+        $modifiedString = $magazine;
+        for ($i = 0;$i < strlen($ransomNote);$i++) {
+            $match = strpos($modifiedString, $ransomNote[$i]);
+            echo $modifiedString . " " . $ransomNote[$i] . " " .$match . " ";
+            if ($match) {
+                if (strlen($modifiedString) > 1) {
+                    $modifiedString = substr($modifiedString, 0, $i) . substr($modifiedString, $i + 1);
+                }
+                // echo $modifiedString;
+                continue;
+            }
+            return false;
+        }
+        return true;
+          
         
     }
 }
 // @lc code=end
+// $ransomNote = "a";
+// $magazine = "b";
+// $ransomNote = "aa";
+// $magazine = "ab";
+$ransomNote = "aa";
+$magazine = "aab";
+echo (new Solution())->canConstruct($ransomNote, $magazine);
 
